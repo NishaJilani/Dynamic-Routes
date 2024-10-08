a9a86284fc89c58247ddfbc1d223b85d35e3dece
Nisha Jilani <nisha03240428081@gmail.com>
2024-10-08 19:52:39 +0500

    Adding a dynamic route assignment here

---
+++ app/country/[country_name]/page.tsx
@@ -0,0 +1,57 @@
+
+export default function CountryName({params}:any) {
+
+   const countries: { //array of objects
+    name: string;
+    population: number;
+    capital: string;
+   } [] = [
+    {
+        name: "Pakistan",
+        population: 123400000,
+        capital: "Islamabad",
+    },
+    {
+        name: "Turkey",
+        population: 5876879800,
+        capital: "Ankara",
+    },
+    {
+        name: "India",
+        population: 87452768797,
+        capital: "Delhi",
+    },
+    {
+        name: "Korea",
+        population: 1244576989000,
+        capital: "Seoul",
+    },
+    {
+        name: "Japan",
+        population: 78967642423500,
+        capital: "Tokyo",
+    },
+   ];
+
+function findCountry(country_url: string){
+     return countries.find(country=> country.name.toLowerCase() == country_url.toLowerCase());
+}
+let result = findCountry(params.country_name);
+
+  return (
+    <div>
+      {
+        result ? (
+          <>
+                     <h1>Country Name: {result.name}</h1>          
+                     <h1>Country Capital: {result?.capital}</h1>
+                     <h1>Country Population: {result?.population}</h1>
+          </>
+        ) : (
+          <h1>{params.country_name} Not Found</h1>
+        )
+      }
+
+    </div>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div style={{textAlign: "center", padding: "100px"}}>
+      <h1>Country List</h1>
+
+      <Link href="/country/pakistan" style={{margin: "10px", borderRadius: "10px", border: "2px solid black", 
+        padding: "10px"}}>Pakistan</Link>
+
+      <Link href="/country/turkey" style={{margin: "10px", borderRadius: "10px", border: "2px solid black",
+         padding: "10px"}}>Turkey</Link>
+
+            <Link href="/country/india" style={{margin: "10px", borderRadius: "10px", border: "2px solid black",
+         padding: "10px"}}>India</Link>
+
+      <Link href="/country/korea" style={{margin: "10px", borderRadius: "10px", border: "2px solid black",
+         padding: "10px"}}>Korea</Link>
+
+      <Link href="/country/japan" style={{margin: "10px", borderRadius: "10px", border: "2px solid black",
+         padding: "10px"}}>Japan</Link>
+
+
+    </div>
+  );
+}
